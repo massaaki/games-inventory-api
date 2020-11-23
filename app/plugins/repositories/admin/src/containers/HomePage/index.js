0a3a65a828f12490dded54705e31b416c116a9ee
Massaaki <mauricio.massaaki@gmail.com>
2020-11-23 00:08:46 -0300

    feat: Add custom plugin(repositories)

---
+++ app/plugins/repositories/admin/src/containers/HomePage/index.js
@@ -0,0 +1,51 @@
+import React, { memo, useState, useEffect } from 'react';
+import { Header } from '@buffetjs/custom';
+import { Table } from '@buffetjs/core';
+import styled from 'styled-components';
+import axios from 'axios';
+
+const Wrapper = styled.div`
+  padding: 18px 30px;
+
+  p {
+    margin-top: 1rem;
+  }
+`
+
+const HomePage = () => {
+  const [rows, setRows] = useState([]);
+  const headers = [
+    {
+      name: 'Name',
+      value: 'name',
+    },
+    {
+      name: 'Description',
+      value: 'description',
+    },
+    {
+      name: 'Url',
+      value: 'html_url',
+    },
+  ];
+
+  useEffect(() => {
+    axios
+      .get("https://api.github.com/users/massaaki/repos")
+      .then(res => setRows(res.data))
+      .catch(e => ExtensionScriptApis.notification.error(`Ops... github API error, ${e}`));
+  },[])
+
+  return (
+    <Wrapper>
+      <Header
+        title={{ label: 'Repositories' }}
+        content='A list of repositories'
+      />
+
+      <Table headers={headers} rows={rows} />
+    </Wrapper>
+  );
+};
+
+export default memo(HomePage);
